46472832cf119b28e092ad0e39428a341f7bd44e
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2018-05-11 23:23:54 +0900

    work_tmp

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	"development": {
 		"define": {
-			"API_ENDPOINT": JSON.stringify("http://127.0.0.1:3014/api/v1/hw_manager"),
+			"API_ENDPOINT": JSON.stringify("http://mochiya98.starfree.jp/api/v1/hw_manager"),
+			"API_ENDPOINT": JSON.stringify("https://ss1.xrea.com/wvagc.s1001.xrea.com/api/v1/hw_manager"),
 		},
 	},
 	"production": {
 		"define": {
-			"API_ENDPOINT": JSON.stringify("http://127.0.0.1:3014/api/v1/hw_manager"),
+			"API_ENDPOINT": JSON.stringify("https://ss1.xrea.com/wvagc.s1001.xrea.com/api/v1/hw_manager"),
 		},
 	},
 };
---
+++ lunchpp.js
@@ -0,0 +1,1 @@
+require("puppeteer").launch({headless: false})
